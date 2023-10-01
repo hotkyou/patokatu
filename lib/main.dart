195b74ff50164dc086f0199b5a2477ceb2b3b8b7
hotkyou <hottarakasi.stu@gmail.com>
2023-10-01 12:01:10 +0900

    add: LineLogin

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
-import 'screens/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+import 'screens/login.dart';
+import 'camera/camera.dart';
+import 'authmanage.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  await Firebase.initializeApp(); // firebaseの初期化
+  // ↓に先ほど作成したチャンネルのチャンネルIDを入れる(String)
+  await LineSDK.instance.setup("2000955560");
+  runApp(
+    // riverpod用の記述
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authManager = ref.watch(authManagerProvider);
     return MaterialApp(
-      title: 'MyApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme: ThemeData.dark(),
-      home: const Login(),
+      debugShowCheckedModeBanner: false,
+      title: 'タイトル',
+      // ログイン中：ホーム画面、未ログイン：ログイン画面
+      home: authManager.isLoggedIn ? const CameraPage() : const Login(),
     );
   }
 }
---
+++ lib/screens/point.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PointPage extends StatelessWidget {
+  const PointPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ポイント')),
+      body: const Center(
+        child: Text('0'),
+      ),
+    );
+  }
+}
